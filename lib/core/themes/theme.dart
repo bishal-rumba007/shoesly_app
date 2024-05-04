f733291e1f493f3207021f228972d3c99bbd913f
bishal.rumba007 <bishal.rumba007@gmail.com>
2024-05-04 07:07:31 +0545

    create new themes

--- lib/core/themes/theme.dart
+++ lib/core/themes/theme.dart
@@ -14,10 +14,68 @@ ThemeData lightMode = ThemeData(
     onSurface: AppColor.grey300,
   ),
 
-  appBarTheme: const AppBarTheme(
+  iconTheme: const IconThemeData(
+    color: AppColor.primaryColor,
+  ),
+  appBarTheme: AppBarTheme(
     backgroundColor: Colors.white,
     elevation: 0,
-    iconTheme: IconThemeData(color: AppColor.primaryColor),
+    titleTextStyle: TextStyle(
+      fontSize: 16.sp,
+      fontWeight: FontWeight.w600,
+      color: Colors.black,
+    ),
+    iconTheme: const IconThemeData(color: AppColor.primaryColor),
+  ),
+  fontFamily: "Urbanist",
+
+  /// Text Theme
+  textTheme: TextTheme(
+      titleLarge: TextStyle(
+        fontSize: 24.sp,
+        fontWeight: FontWeight.w600,
+        color: Colors.black,
+      ),
+      titleMedium: TextStyle(
+        fontSize: 22.sp,
+        fontWeight: FontWeight.w600,
+        color: Colors.black,
+      ),
+      titleSmall: TextStyle(
+        fontSize: 20.sp,
+        fontWeight: FontWeight.w600,
+        color: Colors.black,
+      ),
+      bodyLarge: TextStyle(
+        fontSize: 18.sp,
+        fontWeight: FontWeight.w600,
+        color: Colors.black,
+      ),
+      bodyMedium: TextStyle(
+        fontSize: 16.sp,
+        fontWeight: FontWeight.w500,
+        color: Colors.black,
+      ),
+      bodySmall: TextStyle(
+        fontSize: 14.sp,
+        fontWeight: FontWeight.w400,
+        color: Colors.black,
+      ),
+      labelLarge: TextStyle(
+        fontSize: 14.sp,
+        fontWeight: FontWeight.w600,
+        color: Colors.black,
+      ),
+      labelMedium: TextStyle(
+        fontSize: 12.sp,
+        fontWeight: FontWeight.w500,
+        color: Colors.black,
+      ),
+      labelSmall: TextStyle(
+        fontSize: 10.sp,
+        fontWeight: FontWeight.w400,
+        color: Colors.black,
+      ),
   ),
 
   cardTheme: CardTheme(
@@ -27,11 +85,66 @@ ThemeData lightMode = ThemeData(
     )
   ),
 
+  elevatedButtonTheme: ElevatedButtonThemeData(
+    style: ElevatedButton.styleFrom(
+      disabledBackgroundColor: AppColor.primaryColor.withOpacity(0.5),
+      minimumSize: Size(double.infinity, 50.h),
+      textStyle: TextStyle(
+        fontWeight: FontWeight.bold,
+        fontSize: 14.sp,
+        color: Colors.white,
+      ),
+      backgroundColor: AppColor.primaryColor,
+      foregroundColor: Colors.white,
+      elevation: 0,
+      padding: EdgeInsets.symmetric(horizontal: 16.w, vertical: 16.h),
+    ),
+  ),
+
+  outlinedButtonTheme: OutlinedButtonThemeData(
+    style: OutlinedButton.styleFrom(
+      minimumSize: Size(double.infinity, 50.h),
+      textStyle: TextStyle(
+        fontWeight: FontWeight.bold,
+        fontSize: 14.sp,
+        color: AppColor.primaryColor,
+      ),
+      backgroundColor: Colors.white,
+      side: BorderSide(
+        color: AppColor.grey200,
+        width: 1.5.w,
+      ),
+      elevation: 0,
+      padding: EdgeInsets.symmetric(horizontal: 24.w, vertical: 16.h),
+    ),
+
+  ),
+
   floatingActionButtonTheme: FloatingActionButtonThemeData(
     backgroundColor: AppColor.primaryColor,
     shape: RoundedRectangleBorder(
       borderRadius: BorderRadius.all(Radius.circular(30.r)),
     ),
     extendedPadding: EdgeInsets.symmetric(horizontal: 20.w, vertical: 10.h),
+  ),
+
+  bottomSheetTheme: const BottomSheetThemeData(
+    backgroundColor: Colors.white,
+    surfaceTintColor: Colors.white,
+  ),
+
+  sliderTheme: SliderThemeData(
+    activeTrackColor: Colors.black,
+    inactiveTrackColor: AppColor.grey200,
+    overlayColor: Colors.black.withOpacity(0.1),
+    valueIndicatorColor: Colors.transparent,
+    valueIndicatorShape: const PaddleSliderValueIndicatorShape(),
+    valueIndicatorStrokeColor: Colors.transparent,
+    valueIndicatorTextStyle: TextStyle(
+      color: Colors.black,
+      fontWeight: FontWeight.bold,
+      fontSize: 12.sp,
+    ),
+    showValueIndicator: ShowValueIndicator.always,
   ),
 );
